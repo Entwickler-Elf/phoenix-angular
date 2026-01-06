c74d16d9698146f84e6c0311e7eac2c4119aaf14
Mark Glass <mark.glass@gmail.com>
2026-01-06 16:19:19 -0600

    update tests: add `provideRouter` for modules and adjust test cases for `App` and `HeaderComponent`

--- src/app/layout/header/header-component/header-component.spec.ts
+++ src/app/layout/header/header-component/header-component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 
 import { HeaderComponent } from './header-component';
+import {App} from '../../../app';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -8,7 +10,8 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HeaderComponent]
+      imports: [HeaderComponent],
+      providers: [provideRouter([])]
     })
     .compileComponents();
 
@@ -19,5 +22,12 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render title', async () => {
+    const fixture = TestBed.createComponent(App);
+    await fixture.whenStable();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('div.logo-container>span')?.textContent).toContain("Phoenix Encounter");
   });
 });
